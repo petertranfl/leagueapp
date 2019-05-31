aacde653df646a5d656e36b509bbe45ca8cc63a6
Peter Tran <peter.tran@msionline.com>
2019-05-31 17:51:13 -0500

    successful api call to riot servers

--- hello-world/src/components/SummonerProfile.vue
+++ hello-world/src/components/SummonerProfile.vue
@@ -1,39 +1,65 @@
 <template>
   <div class="hello">
     <h1>{{summonerName}}</h1>
+    <p>{{summonerLevel}}</p>
     
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import AWS from 'aws-sdk'
 
 export default {
   name: 'SummonerProfile',
   data() {
     return {
       summonerName: String,
+      summonerLevel: Number,
       loading: false,
-      userSummonerName: 'lolitspetey',
-      apiKey: 'RGAPI-564bf915-7f35-418b-8bd5-5db5bcfaf5da'
+      userSummonerName: "EarthRune"
     }
   },
 
 methods: {
-  getSummonerData() {
+  getNASummonerData() {
     this.loading = true;
-    axios.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + this.userSummonerName + '?api_key=' + this.apiKey)
-      .then((response) => {
-        this.loading = false;
-        console.log(response);
-        this.summonerName = response.name
-      })
+
+    //load AWS credentials
+    AWS.config.region = 'us-east-2';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-2:20fd57cf-7bb6-4352-b5ce-69eca3907336'
+      });
+
+    //create AWS service object
+    var lambda = new AWS.Lambda({region: 'us-east-2'});
+    //create JSON object for parameters for invoking Lambda function
+    var reqParams = {
+        FunctionName: 'getNASummonerDataByName',
+        InvocationType: 'RequestResponse',
+        LogType: 'None',
+        Payload: '{"SummonerName?": ' + `"` + this.userSummonerName + '"}'
+      };
+      //create variable to hold data returned by Lambda function
+    var reqResults;
+
+      //Calls Lambda function 'GetSummonerDataByName' with given reqParams
+    lambda.invoke(reqParams, (error, data) => {
+        if (error) {
+          this.loading = false;
+          prompt(error);
+        } else {
+          this.loading = false;
+          reqResults = JSON.parse(data.Payload)
+          this.summonerName = reqResults.name
+          this.summonerLevel = reqResults.summonerLevel
+        }
+      });
     }
   },
-  created() {
-  this.getSummonerData();
-},
 
+  created() {
+  this.getNASummonerData();
+  },
 }
 </script>
 
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import AWS from 'aws-sdk'
+
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(AWS)
 
 new Vue({
   render: h => h(App),
